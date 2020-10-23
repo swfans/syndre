b3409987a2c2af82a8a51dc640bd7be8731715ad
mefistotelis <mefistotelis@gmail.com>
2020-10-23 16:19:55 +0200

    bflibrary: Added mouse module

--- src/bflib_keybrd.h
+++ src/bflib_keybrd.h
@@ -199,6 +199,8 @@ extern char lbInkeyToAscii[];
 extern char lbInkeyToAsciiShift[];
 
 /******************************************************************************/
+int LbKeyboardOpen(void);
+int LbIKeyboardOpen(void);
 int LbKeyboardClose(void);
 int LbKeyboard(void);
 /******************************************************************************/
