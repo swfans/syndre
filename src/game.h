5b710fe0fb627735b086284478432301d2a7428a
mefistotelis <mefistotelis@gmail.com>
2022-05-25 15:09:04 +0200

    bfgame: Use extern C

--- src/game.h
+++ src/game.h
@@ -4,6 +4,10 @@
 #include <stdbool.h>
 #include <stddef.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 bool game_initialise (void);
 void game_handle_sdl_events (void);
 void game_update (void);
@@ -11,5 +15,9 @@ void game_quit (void);
 void game_transform_path (const char *file_name, char *result);
 const char *game_get_data_path (void);
 const char *game_get_user_path (void);
+
+#ifdef __cplusplus
+};
+#endif
 
 #endif
