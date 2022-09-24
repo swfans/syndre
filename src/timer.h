3d1e56168c3e2ac09970fcc9c73de199765b9518
mefistotelis <mefistotelis@gmail.com>
2022-09-24 23:35:02 +0200

    bfsoundlib: Remade some timer functions

--- src/timer.h
+++ src/timer.h
@@ -23,13 +23,6 @@
 
 /******************************************************************************/
 
-#define TIMER_MAX_CALLBACKS 16
-
-typedef void (*TimerCallback) (void *user_data);
-typedef int32_t TimerId;
-
-TimerId timer_register_callback (TimerCallback fn);
-void *timer_set_user_data (TimerId timer, void *user_data);
 
 /******************************************************************************/
 #endif // TIMER_H
