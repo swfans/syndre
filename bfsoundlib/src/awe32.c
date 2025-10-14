swfans/swars

bfsoundlib: Moved some variables declaration to C

bfsoundlib/src/awe32.c
@@ -29,6 +29,17 @@
 #include "snderr.h"
 #include "bffile.h"
 /******************************************************************************/
+TbBool UseCurrentAwe32Soundfont = false;
+TbBool Awe32SoundfontLoaded = false;
+
+char CurrentAwe32SoundfontPrefix[12] = "Bullfrog";
+TbFileHandle sbkHandle = INVALID_FILE;
+
+uint8_t *awe_buffer = NULL;
+uint16_t awe_buffer_sel = 0;
+uint8_t *awe_preset = NULL;
+uint16_t awe_preset_sel = 0;
+
 extern TbBool MusicAble;
 extern TbBool MusicInstalled;
 extern char MusicType[6];
@@ -36,13 +47,6 @@ extern AIL_INI MusicInstallChoice;
 extern MDI_DRIVER *MusicDriver;
 extern char SoundDataPath[144];
 
-extern char CurrentAwe32SoundfontPrefix[12]; // = "Bullfrog";
-extern TbFileHandle sbkHandle; // = INVALID_FILE;
-
-extern uint8_t *awe_buffer;
-extern uint16_t awe_buffer_sel;
-extern uint8_t *awe_preset;
-extern uint16_t awe_preset_sel;
 /******************************************************************************/
 
 void FreeAwe32Soundfont(void)

bfsoundlib/src/awe32use.c
@@ -27,11 +27,11 @@
 #include "dpmi.h"
 #include "aildebug.h"
 /******************************************************************************/
-/** Global variable used for transferring parameters to MDI driver. */
-extern SF_DATA *awe_data;
 
+/** Global variable used for transferring parameters to MDI driver. */
+SF_DATA *awe_data = NULL;
 /** DOS selector for awe_data memory block allocation. */
-extern uint16_t awe_data_sel;
+uint16_t awe_data_sel = 0;
 
 /******************************************************************************/
 

swfans/syndre

bfsoundlib/src/awe32.c
@@ -28,6 +28,17 @@
 #include "dpmi.h"
 #include "bffile.h"
 /******************************************************************************/
+TbBool UseCurrentAwe32Soundfont = false;
+TbBool Awe32SoundfontLoaded = false;
+
+char CurrentAwe32SoundfontPrefix[12] = "Bullfrog";
+TbFileHandle sbkHandle = INVALID_FILE;
+
+uint8_t *awe_buffer = NULL;
+uint16_t awe_buffer_seg = 0;
+uint8_t *awe_preset = NULL;
+uint16_t awe_preset_seg = 0;
+
 extern TbBool MusicAble;
 extern TbBool MusicInstalled;
 extern char MusicType[6];
@@ -35,13 +46,6 @@ extern AIL_INI MusicInstallChoice;
 extern MDI_DRIVER *MusicDriver;
 extern char SoundDataPath[144];
 
-extern char CurrentAwe32SoundfontPrefix[12]; // = "Bullfrog";
-extern TbFileHandle sbkHandle; // = INVALID_FILE;
-
-extern uint8_t *awe_buffer;
-extern uint16_t awe_buffer_seg;
-extern uint8_t *awe_preset;
-extern uint16_t awe_preset_seg;
 /******************************************************************************/
 
 void FreeAwe32Soundfont(void)

bfsoundlib/src/awe32use.c
@@ -27,8 +27,11 @@
 #include "dpmi.h"
 #include "aildebug.h"
 /******************************************************************************/
-extern SF_DATA *awe_data;
-extern uint16_t awe_data_seg;
+
+/** Global variable used for transferring parameters to MDI driver. */
+SF_DATA *awe_data = NULL;
+/** DOS selector for awe_data memory block allocation. */
+uint16_t awe_data_seg = 0;
 
 /******************************************************************************/
 